f9407646347c554f7fbecb831d193022f9aad2fd
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-01-25 21:37:00 +0100

    hotfix style deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import styleImport from 'vite-plugin-style-import';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  plugins: [
+    styleImport({
+    }),
+  ],
+};

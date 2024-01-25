91a316402fe7a1cf6fcdd9790c079488ebb754ff
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-01-25 21:29:48 +0100

    install dependances en developement

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import VitePluginScss from 'vite-plugin-scss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginScss(), // Ajoutez cette ligne pour activer le support SCSS
+  ],
 })

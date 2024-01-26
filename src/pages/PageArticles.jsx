8a7224412466c50d22c87cfa1c4379b078e7eb95
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-01-26 13:14:35 +0100

    fix composant Articles

--- src/pages/PageArticles.jsx
+++ src/pages/PageArticles.jsx
@@ -1,13 +1,11 @@
-import Articles from "../components/articles/articles";
+import Articles from "../components/articles/Articles";
 import { useNavigate } from "react-router-dom";
 
 function PageArticles() {
   const navigate = useNavigate();
 
   return (
-    <Articles
-      onArticleClick={(articleId) => navigate(`/articles/${articleId}`)}
-    />
+    <Articles onArticleClick={(articleId) => navigate(`/articles/${articleId}`)}/>
   );
 }
 

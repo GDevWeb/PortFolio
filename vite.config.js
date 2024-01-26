e4754153dff4df3ddf35fe2a7d0768fde4c37fe9
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-01-26 13:06:12 +0100

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import VitePluginScss from 'vite-plugin-scss';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePluginScss()],
+  plugins: [react()],
 })

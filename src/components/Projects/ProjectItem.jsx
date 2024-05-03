1ecbe26f0fb85c5b661d2856baef7b7b36f03b17
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-05-03 13:14:06 +0200

    refonte de la page projects style et contenu mobile | tablette |desktop => ok

--- src/components/Projects/ProjectItem.jsx
+++ src/components/Projects/ProjectItem.jsx
@@ -1,17 +1,18 @@
 import { Link } from "react-router-dom";
-import "../../sass/projectItem.scss"
+import "../../sass/projectItem.scss";
 
-function ProjectItem({ id, title, picture, description, category }) {
+function ProjectItem({ id, title, picture, resume, description, category }) {
   return (
     <div className="projectItem">
       <Link to={`/projects/${id}`} className="linkProject">
+        <h3 className="projectItemTitle">{title}</h3>
         <figure className="projectPicture">
           <img src={picture} alt={`illustration de ${title}`} />
         </figure>
-        <p className="projectItemTitle">{title}</p>
         <p className="articleContent">{description}</p>
+        <p className="projectResume">{resume}</p>
         <p className="projectTags">
-          <span className="projectLabel">{category}</span> 
+          <span className="projectLabel">{category}</span>
         </p>
       </Link>
     </div>

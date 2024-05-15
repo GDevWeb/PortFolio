bbd515bd553619e1fe6bd4c1d0b99a72d89b7fd3
Æscanor <113015001+Aescanor@users.noreply.github.com>
2024-05-15 21:10:52 +0200

    changement du style background - themeColor - revue de certaines mixins - à perfectionner

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.scss";
 import "./app.scss";
 import Root from "./routes/root";
 import PageHome from "./pages/PageHome";
